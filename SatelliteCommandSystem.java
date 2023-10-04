03ccbd9d3cf56a41fc5d919cbf244782fa2f1a52
prasanna2002ks <146970814+prasanna2002ks@users.noreply.github.com>
2023-10-04 21:54:42 +0530

    Add files via upload
    Getting Commands from user

--- SatelliteCommandSystem.java
+++ SatelliteCommandSystem.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,14 +12,17 @@ class Satellite {
 
     void rotate(String direction) {
         try {
-            if (direction.equals("North") || direction.equals("South") || direction.equals("East") || direction.equals("West")) {
+            if (direction.equalsIgnoreCase("North") || direction.equalsIgnoreCase("South") || direction.equalsIgnoreCase("East") || direction.equalsIgnoreCase("West")) {
                 orientation = direction;
-                LOGGER.log(Level.INFO, "Satellite orientation set to " + direction);
+				System.out.println("\n");
+				//LOGGER.log(Level.INFO, "Satellite orientation set to " + direction);
+                LOGGER.info("\nSatellite orientation set to " + direction);
             } else {
-                throw new IllegalArgumentException("Invalid direction. Use North, South, East, or West.");
+                throw new IllegalArgumentException("!!!Invalid direction. Use North, South, East, or West.!!!");
             }
         } catch (IllegalArgumentException e) {
-            LOGGER.log(Level.WARNING, e.getMessage());
+			//LOGGER.log(Level.WARNING, e.getMessage());
+            LOGGER.warning(e.getMessage());  //shorthand code for above line.
         }
     }
 
@@ -26,12 +30,15 @@ class Satellite {
         try {
             if (solarPanels.equals("Inactive")) {
                 solarPanels = "Active";
-                LOGGER.log(Level.INFO, "Solar panels activated.");
+				System.out.println("\n");
+                //LOGGER.log(Level.INFO, "\nSolar panels activated.");
+				LOGGER.info("\nSolar panels activated.");
             } else {
-                throw new IllegalStateException("Solar panels are already active.");
+                throw new IllegalStateException("!!!Solar panels are already active.!!!");
             }
         } catch (IllegalStateException e) {
-            LOGGER.log(Level.WARNING, e.getMessage());
+            //LOGGER.log(Level.WARNING, e.getMessage());
+			LOGGER.warning(e.getMessage());
         }
     }
 
@@ -39,12 +46,15 @@ class Satellite {
         try {
             if (solarPanels.equals("Active")) {
                 solarPanels = "Inactive";
-                LOGGER.log(Level.INFO, "Solar panels deactivated.");
+				System.out.println("\n");
+                //LOGGER.log(Level.INFO, "\nSolar panels deactivated.");
+				LOGGER.info("\nSolar panels deactivated.");
             } else {
-                throw new IllegalStateException("Solar panels are already inactive.");
+                throw new IllegalStateException("!!!Solar panels are already inactive.!!!");
             }
         } catch (IllegalStateException e) {
-            LOGGER.log(Level.WARNING, e.getMessage());
+            //LOGGER.log(Level.WARNING, e.getMessage());
+			LOGGER.warning(e.getMessage());
         }
     }
 
@@ -52,12 +62,15 @@ class Satellite {
         try {
             if (solarPanels.equals("Active")) {
                 dataCollected += 10;
-                LOGGER.log(Level.INFO, "Data collected: " + dataCollected);
+				System.out.println("\n");
+                //LOGGER.log(Level.INFO, "\nData collected : 10");
+				LOGGER.info("\nData collected : 10");
             } else {
-                throw new IllegalStateException("Cannot collect data. Solar panels are inactive.");
+                throw new IllegalStateException("!!!Cannot collect data. Solar panels are inactive.!!!");
             }
         } catch (IllegalStateException e) {
-            LOGGER.log(Level.WARNING, e.getMessage());
+            //LOGGER.log(Level.WARNING, e.getMessage());
+			LOGGER.warning(e.getMessage());
         }
     }
 }
@@ -65,13 +78,56 @@ class Satellite {
 class SatelliteCommandSystem {
     public static void main(String[] args) {
         Satellite satellite = new Satellite();
-        try {
-            satellite.rotate("South");
-            satellite.activatePanels();
-            satellite.collectData();
-            satellite.activatePanels(); // Try activating panels again (should trigger an exception)
-        } catch (Exception e) {
-            e.printStackTrace();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\n\nSatellite Command System");
+        System.out.println("\nInitial state:");
+        printSatelliteState(satellite);
+
+        while (true) {
+            System.out.println("\nEnter a command (rotate, activatePanels, deactivatePanels, collectData, exit):");
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("exit")) {
+                break;
+            }
+
+            try {
+                executeCommand(satellite, input);
+            } catch (IllegalArgumentException e) {
+                System.out.println("!!!Invalid command. Please enter a valid command.!!!");
+            }
+
+            printSatelliteState(satellite);
         }
+    }
+
+    private static void executeCommand(Satellite satellite, String command) {
+		Scanner scanner = new Scanner(System.in);
+		switch(command.toLowerCase()) {
+			case "rotate":
+				System.out.print("Enter Direction to Rotate : ");
+				String direction = scanner.nextLine();
+				satellite.rotate(direction);
+				break;
+			case "activatepanels":
+				satellite.activatePanels();
+				break;
+			case "deactivatepanels":
+				satellite.deactivatePanels();
+				break;
+			case "collectdata":
+				satellite.collectData();
+				break;
+			default:
+				throw new IllegalArgumentException("!!!Invalid command.!!!");
+		}
+    }
+
+    private static void printSatelliteState(Satellite satellite) {
+        System.out.println("\nSatellite state:");
+        System.out.println("Orientation: " + satellite.orientation);
+        System.out.println("Solar Panels: " + satellite.solarPanels);
+        System.out.println("Data Collected: " + satellite.dataCollected);
     }
 }
