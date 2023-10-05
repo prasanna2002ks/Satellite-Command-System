0377b75ec6c02b97828381495d21220ab07adf7f
prasanna2002ks <146970814+prasanna2002ks@users.noreply.github.com>
2023-10-05 08:01:58 +0530

    Add files via upload
    implemented design patterns to the code and encapsulation.

--- SatelliteCommandSystem.java
+++ SatelliteCommandSystem.java
@@ -2,6 +2,66 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+// Command interface
+interface Command {
+    void execute();
+}
+
+// Concrete command classes
+class RotateCommand implements Command {
+    private Satellite satellite;
+    private String direction;
+
+    public RotateCommand(Satellite satellite, String direction) {
+        this.satellite = satellite;
+        this.direction = direction;
+    }
+
+    @Override
+    public void execute() {
+        satellite.rotate(direction);
+    }
+}
+
+class ActivatePanelsCommand implements Command {
+    private Satellite satellite;
+
+    public ActivatePanelsCommand(Satellite satellite) {
+        this.satellite = satellite;
+    }
+
+    @Override
+    public void execute() {
+        satellite.activatePanels();
+    }
+}
+
+class DeactivatePanelsCommand implements Command {
+    private Satellite satellite;
+
+    public DeactivatePanelsCommand(Satellite satellite) {
+        this.satellite = satellite;
+    }
+
+    @Override
+    public void execute() {
+        satellite.deactivatePanels();
+    }
+}
+
+class CollectDataCommand implements Command {
+    private Satellite satellite;
+
+    public CollectDataCommand(Satellite satellite) {
+        this.satellite = satellite;
+    }
+
+    @Override
+    public void execute() {
+        satellite.collectData();
+    }
+}
+
 class Satellite {
     String orientation = "North";
     String solarPanels = "Inactive";
@@ -12,17 +72,16 @@ class Satellite {
 
     void rotate(String direction) {
         try {
-            if (direction.equalsIgnoreCase("North") || direction.equalsIgnoreCase("South") || direction.equalsIgnoreCase("East") || direction.equalsIgnoreCase("West")) {
+            if (direction.equalsIgnoreCase("North") || direction.equalsIgnoreCase("South")
+                    || direction.equalsIgnoreCase("East") || direction.equalsIgnoreCase("West")) {
                 orientation = direction;
-				System.out.println("\n");
-				//LOGGER.log(Level.INFO, "Satellite orientation set to " + direction);
+                System.out.println("\n");
                 LOGGER.info("\nSatellite orientation set to " + direction);
             } else {
                 throw new IllegalArgumentException("!!!Invalid direction. Use North, South, East, or West.!!!");
             }
         } catch (IllegalArgumentException e) {
-			//LOGGER.log(Level.WARNING, e.getMessage());
-            LOGGER.warning(e.getMessage());  //shorthand code for above line.
+            LOGGER.warning(e.getMessage());
         }
     }
 
@@ -30,15 +89,13 @@ class Satellite {
         try {
             if (solarPanels.equals("Inactive")) {
                 solarPanels = "Active";
-				System.out.println("\n");
-                //LOGGER.log(Level.INFO, "\nSolar panels activated.");
-				LOGGER.info("\nSolar panels activated.");
+                System.out.println("\n");
+                LOGGER.info("\nSolar panels activated.");
             } else {
                 throw new IllegalStateException("!!!Solar panels are already active.!!!");
             }
         } catch (IllegalStateException e) {
-            //LOGGER.log(Level.WARNING, e.getMessage());
-			LOGGER.warning(e.getMessage());
+            LOGGER.warning(e.getMessage());
         }
     }
 
@@ -46,15 +103,13 @@ class Satellite {
         try {
             if (solarPanels.equals("Active")) {
                 solarPanels = "Inactive";
-				System.out.println("\n");
-                //LOGGER.log(Level.INFO, "\nSolar panels deactivated.");
-				LOGGER.info("\nSolar panels deactivated.");
+                System.out.println("\n");
+                LOGGER.info("\nSolar panels deactivated.");
             } else {
                 throw new IllegalStateException("!!!Solar panels are already inactive.!!!");
             }
         } catch (IllegalStateException e) {
-            //LOGGER.log(Level.WARNING, e.getMessage());
-			LOGGER.warning(e.getMessage());
+            LOGGER.warning(e.getMessage());
         }
     }
 
@@ -62,15 +117,13 @@ class Satellite {
         try {
             if (solarPanels.equals("Active")) {
                 dataCollected += 10;
-				System.out.println("\n");
-                //LOGGER.log(Level.INFO, "\nData collected : 10");
-				LOGGER.info("\nData collected : 10");
+                System.out.println("\n");
+                LOGGER.info("\nData collected : 10");
             } else {
                 throw new IllegalStateException("!!!Cannot collect data. Solar panels are inactive.!!!");
             }
         } catch (IllegalStateException e) {
-            //LOGGER.log(Level.WARNING, e.getMessage());
-			LOGGER.warning(e.getMessage());
+            LOGGER.warning(e.getMessage());
         }
     }
 }
@@ -93,7 +146,8 @@ class SatelliteCommandSystem {
             }
 
             try {
-                executeCommand(satellite, input);
+                Command command = createCommand(satellite, input);
+                command.execute();
             } catch (IllegalArgumentException e) {
                 System.out.println("!!!Invalid command. Please enter a valid command.!!!");
             }
@@ -102,26 +156,22 @@ class SatelliteCommandSystem {
         }
     }
 
-    private static void executeCommand(Satellite satellite, String command) {
-		Scanner scanner = new Scanner(System.in);
-		switch(command.toLowerCase()) {
-			case "rotate":
-				System.out.print("Enter Direction to Rotate : ");
-				String direction = scanner.nextLine();
-				satellite.rotate(direction);
-				break;
-			case "activatepanels":
-				satellite.activatePanels();
-				break;
-			case "deactivatepanels":
-				satellite.deactivatePanels();
-				break;
-			case "collectdata":
-				satellite.collectData();
-				break;
-			default:
-				throw new IllegalArgumentException("!!!Invalid command.!!!");
-		}
+    private static Command createCommand(Satellite satellite, String command) {
+        Scanner scanner = new Scanner(System.in);
+        switch (command.toLowerCase()) {
+            case "rotate":
+                System.out.print("Enter Direction to Rotate : ");
+                String direction = scanner.nextLine();
+                return new RotateCommand(satellite, direction);
+            case "activatepanels":
+                return new ActivatePanelsCommand(satellite);
+            case "deactivatepanels":
+                return new DeactivatePanelsCommand(satellite);
+            case "collectdata":
+                return new CollectDataCommand(satellite);
+            default:
+                throw new IllegalArgumentException("!!!Invalid command.!!!");
+        }
     }
 
     private static void printSatelliteState(Satellite satellite) {
